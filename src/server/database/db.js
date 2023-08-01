3ee56393dceb1f50a11e42cd661d7c37063655ae
Shou9.123.Myko <mikofernando2003@gmail.com>
2023-08-01 22:00:36 +0700

    fix backend cashier apps

--- src/server/database/db.js
+++ src/server/database/db.js
@@ -9,7 +9,7 @@ const mysql2 = require("mysql2");
 // });
 
 const connection = mysql2.createConnection({
-  host: "https://api-cashier-app.cyclic.cloud",
+  host: "api.cashier-app.mikofirnando.my.id",
   user: "mikofirn_admin",
   password: "og,zkZ%sK9FC",
   database: "mikofirn_cashier_app",
