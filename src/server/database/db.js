b3120c73e24840cae9632f6ce58951f13bc4aade
Shou9.123.Myko <mikofernando2003@gmail.com>
2023-08-05 20:17:43 +0700

    wkkw

--- src/server/database/db.js
+++ src/server/database/db.js
@@ -1,20 +1,20 @@
 const mysql2 = require("mysql2");
 
-const connection = mysql2.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "cashier_app",
-  multipleStatements: true,
-});
-
 // const connection = mysql2.createConnection({
-//   host: "api.cashier-app.mikofirnando.my.id",
-//   user: "mikofirn_admin",
-//   password: "og,zkZ%sK9FC",
-//   database: "mikofirn_cashier_app",
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "cashier_app",
 //   multipleStatements: true,
 // });
+
+const connection = mysql2.createConnection({
+  host: "api.cashier-app.mikofirnando.my.id",
+  user: "mikofirn_admin",
+  password: "og,zkZ%sK9FC",
+  database: "mikofirn_cashier_app",
+  multipleStatements: true,
+});
 
 connection.connect((err) => {
   if (err) {
